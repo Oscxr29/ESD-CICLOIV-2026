04a9666ecb935538f9712aeb5ad8892f3ecc1108
Oscxr29 <oscarfabricioguevara2019@gmail.com>
2026-07-31 20:03:37 -0600

    correcion de Py

--- Ciclo4/computo1/CLASES/CL-03/variabeles.py
+++ Ciclo4/computo1/CLASES/CL-03/variabeles.py
@@ -1,6 +1,6 @@
 
-nombre = "Juan Perez"
-edad = 32
+nombre = "Juan Patroclo"
+edad = 25
 Nacionalidad = "Cabo Verde"
 
 print("Hola, Yo soy: " + nombre + " tengo " + str(edad) + " años y soy de " + Nacionalidad)
